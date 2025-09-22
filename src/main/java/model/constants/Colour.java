6e41c8c6c115bb4dc4b49939419141c0e6cdb5a2
kukubox <akashaschneider@yandex.ru>
2025-09-22 13:44:30 +0300

    edit Colour,Discount classes, edit pom.xml

--- src/main/java/model/constants/Colour.java
+++ src/main/java/model/constants/Colour.java
@@ -1,6 +1,6 @@
 package model.constants;
 
-public abstract class Colour {
+public class Colour {
 
     public static final String GREEN = "green";
     public static final String RED = "red";
--- src/main/java/model/constants/Discount.java
+++ src/main/java/model/constants/Discount.java
@@ -1,6 +1,6 @@
 package model.constants;
 
-public abstract class Discount {
+public class Discount {
     public static final double COMMON_DISCOUNT = 0;
     public static final double RED_APPLE_DISCOUNT = 60;
 
